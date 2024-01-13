21471ca26638e4263668f6c264270c7c8c035545
tomer oz <no@way.com>
2024-01-13 20:06:10 +0200

    last try al emet

--- script/run-docker-image.sh
+++ script/run-docker-image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker pull tomeroz802/flask-personal-project:22
-docker run -itd --name tomer-codedeploy-task5 -p 5000:5000 tomeroz802/flask-personal-project:22
+sudo docker pull tomeroz802/flask-personal-project:22
+sudo docker run -itd --name tomer-codedeploy-task5 -p 5000:5000 tomeroz802/flask-personal-project:22
